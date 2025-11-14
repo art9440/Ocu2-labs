b153ba23fef1b6b6be7c1a7a91453210e36def09
art9440 <art9440@gmail.com>
2025-11-15 01:19:53 +0700

    Made context for scheduler and make queue for ready threads

--- 1.7/uthread.c
+++ 1.7/uthread.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/ucontext.h>
 #define _GNU_SOURCE
 #include "uthread.h"
@@ -28,3 +30,31 @@ struct uthread {
 };
 
 
+static uthread_t *runq[UTHREAD_MAX];
+static int rq_head = 0, rq_tail = 0;
+
+static int rq_empty(void) {return rq_head == rq_tail;}
+static int rq_next(int x) {return (x + 1) % UTHREAD_MAX;}
+
+static void rq_push(uthread_t *t) {
+    int nt = rq_next(rq_tail);
+    if (nt == rq_head) {
+        fprintf(stderr, "uthread: run queue overflow\n");
+        abort();
+    }
+    runq[rq_tail] = t;
+    rq_tail = nt; 
+}
+
+static uthread_t *rq_pop(void) {
+    if (rq_empty()) return NULL;
+
+    uthread_t *t = runq[rq_head];
+    rq_head = rq_next(rq_head);
+
+    return t;
+}
+
+static ucontext_t sched_ctx;
+static ucontext_t main_ctx;
+static uthread_t *current_ctx = NULL;
