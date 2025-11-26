b54bcd07e9bef6a9d1a3074fef4e71a403eb8d1b
art9440 <art9440@gmail.com>
2025-11-26 13:33:15 +0700

    Made .h file for storage with mutext for sync

---
+++ 2.3/mutex/storage.h
@@ -0,0 +1,32 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+
+#include <pthread.h>
+
+typedef struct _Node {
+    char value[100];
+    struct _Node* next;
+    pthread_mutex_t sync;
+} Node;
+
+
+typedef struct _Storage {
+    Node *first;
+} Storage;
+
+
+void init_storage(Storage* storage);
+
+Node* create_node(const char* value);
+
+void add_to_storage(Storage* storage, const char* value);
+
+void fill_storage_random(Storage* storage, int count);
+
+void free_storage(Storage* storage);
+
+void generate_random_string(char* buffer, int max_length);
+
+int string_length(const char* str);
+
+#endif
