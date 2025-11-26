4e5f341d828c6351362a23e074801bd9794bea48
art9440 <art9440@gmail.com>
2025-11-26 20:06:18 +0700

    finished main

--- 2.3/mutex/main_mtx.c
+++ 2.3/mutex/main_mtx.c
@@ -1,4 +1,5 @@
 #include "threads.h"
+#include <bits/pthreadtypes.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <time.h>
@@ -17,10 +18,18 @@ int main(){
 
     pthread_t asc_thread, desc_thread, equal_thread;
     pthread_t swap_thread1, swap_thread2, swap_thread3;
+    pthread_t monitor_thread;
 
     pthread_create(&asc_thread, NULL, find_ascending_pairs, &storage);
     pthread_create(&desc_thread, NULL, find_descending_pairs, &storage);
     pthread_create(&equal_thread, NULL, find_equal_pairs, &storage);
+
+    pthread_create(&swap_thread1, NULL, swap_asc_thread, &storage);
+    pthread_create(&swap_thread2, NULL, swap_desc_thread, &storage);
+    pthread_create(&swap_thread3, NULL, swap_equal_thread, &storage);
+
+
+    pthread_create(&monitor_thread, NULL, thread_monitor, NULL);
 
     free_storage(&storage);
     pthread_mutex_destroy(&print_mutex);
