ca45091bac1b074df65f8f86b734aeba4efc1f51
art9440 <art9440@gmail.com>
2025-11-28 05:00:37 +0700

    Started spinlock list

---
+++ 2.3/spin/storage.h
@@ -0,0 +1,34 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+
+#include <pthread.h>
+
+typedef struct _Node {
+    char value[100];
+    struct _Node* next;
+    pthread_spinlock_t sync; 
+} Node;
+
+
+typedef struct _Storage {
+    Node *first;
+    int count;
+    volatile int stop;
+} Storage;
+
+
+void init_storage(Storage* storage);
+
+Node* create_node(const char* value);
+
+void add_to_storage(Storage* storage, const char* value);
+
+void fill_storage_random(Storage* storage, int count);
+
+void free_storage(Storage* storage);
+
+void generate_random_string(char* buffer, int max_length);
+
+int string_length(const char* str);
+
+#endif
